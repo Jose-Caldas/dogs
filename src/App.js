a3c48b124288a0f88d7f3e8e8ee3f124a969fb6c
José Caldas <jrcaldas@yahoo.com.br>
2022-09-16 22:10:01 -0300

    feat/add initial structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="container">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <h1>FOOTER</h1>
+    </div>
+  );
+};
+
+export default Footer;
