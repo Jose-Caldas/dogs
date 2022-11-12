eb9f2f97d5f96085fd1084f72a3453961d7e9173
José Caldas <jrcaldas@yahoo.com.br>
2022-11-12 19:01:09 -0300

    feat: update LoginCreate

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
+import useForm from "../../Hooks/useForm";
+import { USER_POST } from "../../Api";
+import { UserContext } from "../../UserContext";
 
 const LoginCreate = () => {
-  return <div>Login criar</div>;
+  const username = useForm();
+  const email = useForm("email");
+  // const password = useForm("password"); // com regex para uma senha mais segura
+  const password = useForm(); //senha simples
+  const { userLogin } = useContext(UserContext);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+    console.log("response", response);
+  };
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastrar-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input type="text" label="Usuário" name="username" {...username} />
+        <Input type="email" label="Email" name="email" {...email} />
+        <Input type="password" label="Senha" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
