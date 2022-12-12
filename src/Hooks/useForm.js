f2acbaa3a3031c71d01909aa2e1dfe046c238255
José Caldas <jrcaldas@yahoo.com.br>
2022-12-12 20:37:56 -0300

    feat: added photo post

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -11,6 +11,10 @@ const types = {
     message:
       "A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres",
   },
+  number: {
+    regex: /^\d+$/,
+    message: "Utilize apenas números",
+  },
 };
 
 const useForm = (type) => {
