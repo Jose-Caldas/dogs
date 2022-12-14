b92476b259eba1572bc4c643d97edf22c70db7e4
José Caldas <jrcaldas@yahoo.com.br>
2022-12-14 18:03:21 -0300

    feat: feed initial implementation

---
+++ src/Components/Feed/FeedPhotos.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { PHOTOS_GET } from "../../Api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loding";
+import styles from "./FeedPhotos.module.css";
+import FeedPhotosItem from "./FeedPhotosItem";
+
+const FeedPhotos = () => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { json } = await request(url, options);
+      console.log(json);
+    }
+    fetchPhotos();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+
+  if (data)
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  else return null;
+};
+
+export default FeedPhotos;
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 export const Footer = () => {
   return (
     <div className={styles.footer}>
-      <h1>FOOTER</h1>
+      <h1></h1>
     </div>
   );
 };
