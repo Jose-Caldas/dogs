6aee513e9ba14337b90edbd76ee78524ca7573d1
José Caldas <jrcaldas@yahoo.com.br>
2022-12-17 16:09:01 -0300

    feat: add Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { ReactComponent as Dogs } from "../Assets/dogs-footer.svg";
 
 export const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <h1></h1>
-    </div>
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
   );
 };
 
