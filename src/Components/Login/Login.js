18eaa08e5266047de762d71335746dee11c3b70f
José Caldas <jrcaldas@yahoo.com.br>
2022-12-16 22:30:06 -0300

    add page not found

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,6 +6,7 @@ import LoginForm from "./LoginForm";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginpasswordReset from "./LoginpasswordReset";
 import styles from "./Login.module.css";
+import NotFound from "../NotFound";
 
 const Login = () => {
   const { login } = useContext(UserContext);
@@ -20,6 +21,7 @@ const Login = () => {
           <Route path="criar" element={<LoginCreate />} />
           <Route path="perdeu" element={<LoginPasswordLost />} />
           <Route path="reset" element={<LoginpasswordReset />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </section>
--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -2,8 +2,10 @@ import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import Feed from "../Feed/Feed";
+import NotFound from "../NotFound";
 import UserHeader from "./UserHeader";
 import UserPhotoPost from "./UserPhotoPost";
+import UserProfile from "./UserProfile";
 import UserStats from "./UserStats";
 
 const User = () => {
@@ -15,6 +17,7 @@ const User = () => {
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="/postar" element={<UserPhotoPost />} />
         <Route path="/estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
