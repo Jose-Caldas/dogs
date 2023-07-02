bc45a656e8d6da8436ccc9dd65a2ddda63f3b2c6
Jose Caldas <jrcaldas@yahoo.com.br>
2023-07-02 14:22:00 -0300

    fix/yarn start

--- src/Components/Login/LoginpasswordReset.js
+++ src/Components/Login/LoginpasswordReset.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import Input from '../Forms/Input';
-import Button from '../Forms/Button';
-import useForm from '../../Hooks/useForm';
-import useFetch from '../../Hooks/useFetch';
-import { PASSWORD_RESET } from '../../Api';
-import Error from '../Helper/Error';
-import { useNavigate } from 'react-router-dom';
-import Head from '../Helper/Head';
+import React from 'react'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import useForm from '../../Hooks/useForm'
+import useFetch from '../../Hooks/useFetch'
+import { PASSWORD_RESET } from '../../Api'
+import Error from '../Helper/Error'
+import { useNavigate } from 'react-router-dom'
+import Head from '../Helper/Head'
 
 const LoginPasswordReset = () => {
-  const [login, setLogin] = React.useState('');
-  const [key, setKey] = React.useState('');
-  const password = useForm();
-  const { error, loading, request } = useFetch();
-  const navigate = useNavigate();
+  const [login, setLogin] = React.useState('')
+  const [key, setKey] = React.useState('')
+  const password = useForm()
+  const { error, loading, request } = useFetch()
+  const navigate = useNavigate()
 
   React.useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const key = params.get('key');
-    const login = params.get('login');
-    if (key) setKey(key);
-    if (login) setLogin(login);
-  }, []);
+    const params = new URLSearchParams(window.location.search)
+    const key = params.get('key')
+    const login = params.get('login')
+    if (key) setKey(key)
+    if (login) setLogin(login)
+  }, [])
 
   async function handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
     if (password.validate()) {
       const { url, options } = PASSWORD_RESET({
         login,
         key,
         password: password.value,
-      });
-      const { response } = await request(url, options);
-      if (response.ok) navigate('/login');
+      })
+      const { response } = await request(url, options)
+      if (response.ok) navigate('/login')
     }
   }
 
@@ -55,7 +55,7 @@ const LoginPasswordReset = () => {
       </form>
       <Error error={error} />
     </section>
-  );
-};
+  )
+}
 
-export default LoginPasswordReset;
+export default LoginPasswordReset
