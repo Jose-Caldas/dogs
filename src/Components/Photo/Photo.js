0f9f693294f3a17c99f0d5dc38c07945b01736e9
José Caldas <jrcaldas@yahoo.com.br>
2023-07-02 12:58:39 -0300

    update stats

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,31 +1,31 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { PHOTO_GET } from "../../Api";
-import useFetch from "../../Hooks/useFetch";
-import Error from "../Helper/Error";
-import Head from "../Helper/Head";
-import Loading from "../Helper/Loading";
-import PhotoContent from "./PhotoContent";
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { PHOTO_GET } from '../../Api'
+import useFetch from '../../Hooks/useFetch'
+import Error from '../Helper/Error'
+import Head from '../Helper/Head'
+import Loading from '../Helper/Loading'
+import PhotoContent from './PhotoContent'
 
 const Photo = () => {
-  const { id } = useParams();
-  const { data, error, loading, request } = useFetch();
+  const { id } = useParams()
+  const { data, error, loading, request } = useFetch()
 
   useEffect(() => {
-    const { url, options } = PHOTO_GET(id);
-    request(url, options);
-  }, [id, request]);
+    const { url, options } = PHOTO_GET(id)
+    request(url, options)
+  }, [id, request])
 
-  if (error) return <Error error={error} />;
-  if (loading) return <Loading />;
+  if (error) return <Error error={error} />
+  if (loading) return <Loading />
   if (data)
     return (
       <section className="container mainContainer">
         <Head title={data.photo.title} />
         <PhotoContent data={data} single={true} />
       </section>
-    );
-  else return null;
-};
+    )
+  else return null
+}
 
-export default Photo;
+export default Photo
